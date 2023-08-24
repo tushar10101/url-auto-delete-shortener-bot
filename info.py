b85488cf1e45916d3fccdebcdce9d1eddf48a592
Tushar Lokhil <82140525+tushar10101@users.noreply.github.com>
2023-08-25 00:30:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,8 +36,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Tushar:Lokhil@123@cluster0.jjt1iu5.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Telecost")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telecost')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1905727525'))
